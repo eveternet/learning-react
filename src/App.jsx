37e112105ab35694d0f4a9979a2e8be47972c4b8
pigsye <eayancheong@gmail.com>
2025-05-20 10:52:18 +0800

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Link, Route } from "react-router";
+import { BrowserRouter, Link, Route, Routes } from "react-router";
 import Home from "./components/home";
 import About from "./components/about";
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
